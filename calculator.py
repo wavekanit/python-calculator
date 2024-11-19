39eafdf30a74fca6f12b3de5c1581db456eb347f
Kanit Bunyinkgool <120672560+wavekanit@users.noreply.github.com>
2024-11-19 09:15:04 +0000

    fix: some bug

--- calculator.py
+++ calculator.py
@@ -3,24 +3,73 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        # return b - a # false !!
+        return a - b # fix !!
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        # for i in range(b+1): # false !!
+        if a == 0 or b == 0:
+            return result
+        elif b < 0: # fix edge case !
+            a = -a
+            b = -b
+        for i in range(b): # fix !!
             result = self.add(result, a)
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
+        if b == 0: # add edge case !
+            return "undefined"
+        elif a == 0:
+            return result
+        # while a > b: # false !
+        if a > 0 and b > 0: # case both positive
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+        elif a < 0 and b < 0: # case both neg
+            a = -a
+            b = -b
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+        else: # case some val is neg
+            if a < 0 and b > 0:
+                a = -a
+            else:
+                b = -b
+            while a >= b:
+                a = self.subtract(a, b)
+                result -= 1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
+        neg_result = False # bool for check, if divider is neg result will neg
+        if b == 0: # add edge case !
+            return "undefined"
+        elif a == 0:
+            return 0
+        if a > 0 and b > 0: # case both positive
+            while a >= b:
+                a = self.subtract(a, b)
+        elif a < 0 and b < 0: # case both neg
+            neg_result = True
+            a = -a
+            b = -b
+            while a >= b:
+                a = self.subtract(a, b)
+        else: # case some val is neg
+            if a < 0 and b > 0:
+                a = -a
+            else:
+                neg_result = True
+                b = -b
+            while a >= b:
+                a = self.subtract(a, b)
+        if neg_result == True:
+            return -a
         return a
 
 # Example usage:
@@ -28,7 +77,7 @@ if __name__ == "__main__":
     calc = Calculator()
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
-    print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: subtraction: ", calc.subtract(-3, 1))
+    print("Example: multiplication: ", calc.multiply(-3, -4))
+    print("Example: division: ", calc.divide(0, 0))
+    print("Example: modulo: ", calc.modulo(1, 0))
--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,54 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+    def test_add_neg_case(self):
+        self.assertEqual(self.calc.add(-1, -2), -3)
+
+    def test_subt(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+
+    def test_subt_neg_case(self):
+        self.assertEqual(self.calc.subtract(-2, 1), -3)
+
+    def test_multi(self) :
+        self.assertEqual(self.calc.multiply(3, 2), 6)
+
+    def test_multi_neg_case(self):
+        self.assertEqual(self.calc.multiply(3, -2), -6)
+
+    def test_multi_zero_case(self):
+        self.assertEqual(self.calc.multiply(3, 0), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(3, 1), 3)
+
+    def test_divide_neg_case(self):
+        self.assertEqual(self.calc.divide(-3, 1), -3)
+
+    def test_divide_zero_numerator_case(self):
+        self.assertEqual(self.calc.divide(0, -1), 0)
+
+    def test_divide_both_zero_case(self):
+        self.assertEqual(self.calc.divide(0, 0), "undefined")
+
+    def test_divide_zero_divider_case(self):
+        self.assertEqual(self.calc.divide(3, 0), "undefined")
+    
+    def test_modulo(self):
+        self.assertEqual(self.calc.divide(3, 2), 1)
+
+    def test_modulo_zero_divider_case(self):
+        self.assertEqual(self.calc.modulo(3, 0), "undefined")
+    
+    def test_modulo_zero_numerator_case(self):
+        self.assertEqual(self.calc.modulo(0, 1), 0)
+
+    def test_modulo_both_zero_case(self):
+        self.assertEqual(self.calc.modulo(0, 0), "undefined")
+
+    def test_modulo_neg_case(self):
+        self.assertEqual(self.calc.modulo(5, -2), -1)
+
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
